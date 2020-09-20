4bf213fa4a75b7ba7db84b3e613fc38f0a0f5250
aks2161989 <akshay_chavan2003@yahoo.com>
2020-09-20 19:39:04 +0530

    Section 8 exercise completed.

---
+++ Final_Vidly/Models/ApplicationUser.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace Final_Vidly.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        [StringLength(255)]
+        public string DrivingLicense { get; set; }
+
+        [Required]
+        [Display(Name = "Phone")]
+        [StringLength(50, ErrorMessage = "The phone number cannot be longer than 50 characters long")]
+        public string Phone { get; set; }
+
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
+}
---
+++ Final_Vidly/ViewModels/ExternalLoginConfirmationViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Final_Vidly.Models
+{
+    public class ExternalLoginConfirmationViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Driving License")]
+        public string DrivingLicense { get; set; }
+
+        [Required]
+        [Display(Name = "Phone")]
+        [StringLength(50, ErrorMessage = "The phone number cannot be longer than 50 characters long")]
+        public string Phone { get; set; }
+
+    }
+}
